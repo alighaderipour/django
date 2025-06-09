894f00a9e5b4315d1e5c2bfccd9327f3f685674b
Shahaz <ali.ghaderipour@gmail.com>
2025-06-09 12:25:37 +0330

    latest change

--- 2/job_board/urls.py
+++ 2/job_board/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index, job_details
 
 urlpatterns = [
     path('', index, name='home'),
+    path('job/', index, name='home'),
 ]
--- 2/job_board/views.py
+++ 2/job_board/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-
+from .models import  JobPosting
 # Create your views here.
 def index(request):
-    return render(request, 'job_board/index.html', {})
+    job_postings = JobPosting.objects.all()
+    active_job_postings = JobPosting.objects.filter(is_active=True)
+    not_active_job_postings = JobPosting.objects.filter(is_active=False)
+    print(job_postings)
+    context = {'job_postings': job_postings, 'not_active_job_postings': not_active_job_postings, 'active_job_postings': active_job_postings}
+    return render(request, 'job_board/index.html', context)
 
-
+def job_details(request, job_id):
+    job = JobPosting.objects.get(id=job_id)
+    context = {'job': job}
+    return render(request, 'job_board/job_detail.html', context)
